85878b66d5e86227200e1d031a78e35b96aea62d
PetrBodunov <pbod@list.ru>
2024-02-12 14:21:31 +0300

    add backtrace last 10 syscalls

--- main.py
+++ main.py
@@ -9,5 +9,17 @@ for i in range(len(data)-2):
     str = re.sub(r'(.*)(\(.*\))(\s*=\s)(.*)', r'\1;\2;\4', data[i]).split(';')
     dataset.append({'system call': str[0], 'var': str[1], 'result': str[2]})
 
-print(dataset[0])
-print(dataset[1])
+# print(dataset[0])
+# print(dataset[1])
+
+# for i in range(len(dataset)):
+#     print(dataset[i])
+
+last_n_syscalls = 10
+last_syscalls = []
+
+for i in range(len(dataset) - last_n_syscalls - 1):
+    last_syscalls.append({'syscall': dataset[i + last_n_syscalls], 'last syscalls': [dataset[i:i + last_n_syscalls]]})
+
+print(last_syscalls[0])
+print(last_syscalls[1])
