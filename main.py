b91d9f87868a9a7ad62da878281db3a5739b50b5
PetrBodunov <pbod@list.ru>
2024-02-26 18:47:06 +0300

    add compile kernel, firefox, uname syscall dataset

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import re
+import numpy as np
+import os
 
-with open('strace.log', 'r') as file:
-    data = file.read().split('\n')
+s = os.listdir()
+dirs = [f for f in list(filter(os.path.isdir, os.listdir())) if f not in ['venv', '.idea']]
 
-dataset = []
-
-for i in range(len(data)-2):
-    str = re.sub(r'(.*)(\(.*\))(\s*=\s)(.*)', r'\1;\2;\4', data[i]).split(';')
-    dataset.append({'system call': str[0], 'var': str[1], 'result': str[2]})
-
-# print(dataset[0])
-# print(dataset[1])
-
-# for i in range(len(dataset)):
-#     print(dataset[i])
 
 last_n_syscalls = 10
 last_syscalls = []
 
-for i in range(len(dataset) - last_n_syscalls - 1):
-    last_syscalls.append({'syscall': dataset[i + last_n_syscalls], 'last syscalls': [dataset[i:i + last_n_syscalls]]})
+# print(dirs)
+# dirs = ['firefox']
+for dir in dirs:
+    files = os.listdir(dir)
 
-print(last_syscalls[0])
-print(last_syscalls[1])
+    for f in files:
+        print(f)
+        data = []
+        with open(f'{dir}/{f}', 'r') as file:
+            data = file.read().split('\n')
+
+        dataset = []
+        reg_exp = r'(.*) (.*)(\(.*\))(\s*=\s)(.*)'
+
+        for i in range(len(data) - 2):
+            if (bool(re.search(reg_exp, data[i]))):
+                str = re.sub(reg_exp, r'\1;\2;\3;\5', data[i]).split(';')
+                dataset.append({'system call': str[1], 'var': str[2], 'result': str[3], 'time': str[0]})
+
+        for i in range(len(dataset) - last_n_syscalls):
+            last_syscalls.append(
+                {'syscall': dataset[i + last_n_syscalls], 'last syscalls': dataset[i:i + last_n_syscalls]})
+
+for i in range(len(last_syscalls)):
+    print(i + 1, last_syscalls[i])
