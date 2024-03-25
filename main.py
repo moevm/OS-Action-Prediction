7898d15eb050836b8d6caa3785faae981b55e1e6
PetrBodunov <pbod@list.ru>
2024-03-25 18:58:19 +0300

    add syscall prediction network

--- main.py
+++ main.py
@@ -1,36 +1,68 @@
 import re
 import numpy as np
 import os
+import csv
 
-s = os.listdir()
-dirs = [f for f in list(filter(os.path.isdir, os.listdir())) if f not in ['venv', '.idea']]
+path = 'dataset'
 
+dirs = os.listdir(path)
 
-last_n_syscalls = 10
+print(dirs)
+
+last_n_syscalls = 5
 last_syscalls = []
 
 # print(dirs)
-# dirs = ['firefox']
+dirs = ['uname']
 for dir in dirs:
-    files = os.listdir(dir)
+    newpath = f'{path}/{dir}'
+    files = os.listdir(newpath)
 
     for f in files:
-        print(f)
         data = []
-        with open(f'{dir}/{f}', 'r') as file:
+        with open(f'{newpath}/{f}', 'r') as file:
             data = file.read().split('\n')
 
         dataset = []
-        reg_exp = r'(.*) (.*)(\(.*\))(\s*=\s)(.*)'
+        reg_exp = r'(.+?) (.+?)(\(.+\))(\s*=\s*)(.*)'
 
         for i in range(len(data) - 2):
             if (bool(re.search(reg_exp, data[i]))):
                 str = re.sub(reg_exp, r'\1;\2;\3;\5', data[i]).split(';')
-                dataset.append({'system call': str[1], 'var': str[2], 'result': str[3], 'time': str[0]})
+                # with dictionary
+                # dataset.append({'system call': str[1], 'var': str[2], 'result': str[3]})
+
+                # with str
+                print(80*'-')
+                print(f'{str[1]} ; {str[2]} ; {str[3].split()[0]}')
+                print(80*'-')
+
+                dataset.append(f'{str[1]}syscall{str[2]}syscall{str[3].split()[0]}')
+
+                # with time
+                # dataset.append({'system call': str[1], 'var': str[2], 'result': str[3], 'time': str[0]})
 
         for i in range(len(dataset) - last_n_syscalls):
             last_syscalls.append(
-                {'syscall': dataset[i + last_n_syscalls], 'last syscalls': dataset[i:i + last_n_syscalls]})
+                {'syscall': dataset[i + last_n_syscalls], 'last syscalls': 'last_n_syscalls'.join(dataset[i:i + last_n_syscalls])})
 
-for i in range(len(last_syscalls)):
-    print(i + 1, last_syscalls[i])
+
+
+with open('syscalls_dataset.csv', 'w', newline='') as csvfile:
+    fieldnames = ['last syscalls', 'syscall']
+
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+    writer.writeheader()
+    for el in last_syscalls:
+        writer.writerow(el)
+
+
+# print(last_syscalls[0])
+# print(last_syscalls[0]['syscall'])
+# print(last_syscalls[0]['last syscalls'])
+
+# for i in last_syscalls[0]['last syscalls']:
+#     print(i)
+
+# for i in range(len(last_syscalls)):
+#     print(i + 1, last_syscalls[i])
